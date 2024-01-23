78eb8a1813d029f845c563a17a8e277dd14c4b2c
Sayyid Olim <sayyid.olim12@gmail.com>
2024-01-23 16:06:48 +0700

    Add responsive layout example

--- lib/component/list.dart
+++ lib/component/list.dart
@@ -1,5 +1,5 @@
-import 'package:task_intern_2_flutter/import.dart'
-;
+import 'package:task_intern_2_flutter/import.dart';
+
 class ListCommentPage extends StatelessWidget {
   const ListCommentPage({
     super.key,
@@ -43,7 +43,6 @@ class ListCommentPage extends StatelessWidget {
   }
 }
 
-
 class ListUserPage extends StatelessWidget {
   const ListUserPage({
     super.key,
@@ -56,51 +55,189 @@ class ListUserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          final userResult = userData[index];
-          return Card(
-            elevation: 3,
-            child: ListTile(
-              leading: CircleAvatar(
-                child: Text('${userResult.id}'),
-              ),
-              title: Text(
-                '${userResult.name} / (${userResult.userName})',
-              ),
-              subtitle: Text(userResult.email),
-              onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  RoutesName.user,
-                  arguments: {
-                    'id': userResult.id,
-                    'name': userResult.name,
-                    'username': userResult.userName,
-                    'email': userResult.email,
-                    'street': userResult.address!.street,
-                    'suite': userResult.address!.suite,
-                    'city': userResult.address!.city,
-                    'zipcode': userResult.address!.zipcode,
-                    'lat': userResult.address!.geo.lat,
-                    'lng': userResult.address!.geo.lng,
-                    'phone': userResult.phone,
-                    'website': userResult.website,
-                    'companyName': userResult.company!.name,
-                    'catchphrase': userResult.company!.catchPhrase,
-                    'bs': userResult.company!.bs
+      child: LayoutBuilder(builder: (context, constraints) {
+        if (constraints.maxWidth <= 600) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final userResult = userData[index];
+              return Card(
+                elevation: 3,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${userResult.id}'),
+                  ),
+                  title: Text(
+                    '${userResult.name} / (${userResult.userName})',
+                  ),
+                  subtitle: Text(userResult.email),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      RoutesName.user,
+                      arguments: {
+                        'id': userResult.id,
+                        'name': userResult.name,
+                        'username': userResult.userName,
+                        'email': userResult.email,
+                        'street': userResult.address!.street,
+                        'suite': userResult.address!.suite,
+                        'city': userResult.address!.city,
+                        'zipcode': userResult.address!.zipcode,
+                        'lat': userResult.address!.geo.lat,
+                        'lng': userResult.address!.geo.lng,
+                        'phone': userResult.phone,
+                        'website': userResult.website,
+                        'companyName': userResult.company!.name,
+                        'catchphrase': userResult.company!.catchPhrase,
+                        'bs': userResult.company!.bs
+                      },
+                    );
                   },
-                );
-              },
-            ),
+                ),
+              );
+            },
+            itemCount: userData.length,
           );
-        },
-        itemCount: userData.length,
-      ),
+        } 
+        if (constraints.maxWidth <= 840) {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3),
+            itemBuilder: (context, index) {
+              final userResult = userData[index];
+              return Card(
+                elevation: 3,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${userResult.id}'),
+                  ),
+                  title: Text(
+                    '${userResult.name} / (${userResult.userName})',
+                  ),
+                  subtitle: Text(userResult.email),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      RoutesName.user,
+                      arguments: {
+                        'id': userResult.id,
+                        'name': userResult.name,
+                        'username': userResult.userName,
+                        'email': userResult.email,
+                        'street': userResult.address!.street,
+                        'suite': userResult.address!.suite,
+                        'city': userResult.address!.city,
+                        'zipcode': userResult.address!.zipcode,
+                        'lat': userResult.address!.geo.lat,
+                        'lng': userResult.address!.geo.lng,
+                        'phone': userResult.phone,
+                        'website': userResult.website,
+                        'companyName': userResult.company!.name,
+                        'catchphrase': userResult.company!.catchPhrase,
+                        'bs': userResult.company!.bs
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+            itemCount: userData.length,
+          );
+        }
+        if (constraints.maxWidth <= 1000) {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4),
+            itemBuilder: (context, index) {
+              final userResult = userData[index];
+              return Card(
+                elevation: 3,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${userResult.id}'),
+                  ),
+                  title: Text(
+                    '${userResult.name} / (${userResult.userName})',
+                  ),
+                  subtitle: Text(userResult.email),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      RoutesName.user,
+                      arguments: {
+                        'id': userResult.id,
+                        'name': userResult.name,
+                        'username': userResult.userName,
+                        'email': userResult.email,
+                        'street': userResult.address!.street,
+                        'suite': userResult.address!.suite,
+                        'city': userResult.address!.city,
+                        'zipcode': userResult.address!.zipcode,
+                        'lat': userResult.address!.geo.lat,
+                        'lng': userResult.address!.geo.lng,
+                        'phone': userResult.phone,
+                        'website': userResult.website,
+                        'companyName': userResult.company!.name,
+                        'catchphrase': userResult.company!.catchPhrase,
+                        'bs': userResult.company!.bs
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+            itemCount: userData.length,
+          );
+        }
+        else {
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5),
+            itemBuilder: (context, index) {
+              final userResult = userData[index];
+              return Card(
+                elevation: 3,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${userResult.id}'),
+                  ),
+                  title: Text(
+                    '${userResult.name} / (${userResult.userName})',
+                  ),
+                  subtitle: Text(userResult.email),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      RoutesName.user,
+                      arguments: {
+                        'id': userResult.id,
+                        'name': userResult.name,
+                        'username': userResult.userName,
+                        'email': userResult.email,
+                        'street': userResult.address!.street,
+                        'suite': userResult.address!.suite,
+                        'city': userResult.address!.city,
+                        'zipcode': userResult.address!.zipcode,
+                        'lat': userResult.address!.geo.lat,
+                        'lng': userResult.address!.geo.lng,
+                        'phone': userResult.phone,
+                        'website': userResult.website,
+                        'companyName': userResult.company!.name,
+                        'catchphrase': userResult.company!.catchPhrase,
+                        'bs': userResult.company!.bs
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+            itemCount: userData.length,
+          );
+        }
+      }),
     );
   }
 }
-
 
 class ListAlbumPage extends StatelessWidget {
   const ListAlbumPage({
@@ -116,7 +253,9 @@ class ListAlbumPage extends StatelessWidget {
       itemBuilder: (context, index) {
         final albumResult = albumData[index];
         return ListTile(
-          leading: CircleAvatar(child: Text(albumResult.id.toString()),),
+          leading: CircleAvatar(
+            child: Text(albumResult.id.toString()),
+          ),
           title: Text(albumResult.title),
         );
       },
